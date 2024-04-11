93d1758b8bf45dceb50b7d5a53fc86c924337055
Manuel Villen <manuvillen@gmail.com>
2024-04-11 22:22:26 +0200

    Setup 2 comments

--- src/kaboomCtx.js
+++ src/kaboomCtx.js
@@ -1,8 +1,10 @@
 import Kaboom from "kaboom";
 
-export const k = Kaboom({ // Variable global para usar en todo el contexto
+export const k = Kaboom({ // Variable global para usar en todo el contexto.
+    
+    // Properties
     global: false, // Var para no hacer global la constante
     touchToMouse: true, // Var para usar app en movil
-    canvas: document.getElementById("game"),
+    canvas: document.getElementById("game") // Properties, indica el elemento canvas
 });
 
